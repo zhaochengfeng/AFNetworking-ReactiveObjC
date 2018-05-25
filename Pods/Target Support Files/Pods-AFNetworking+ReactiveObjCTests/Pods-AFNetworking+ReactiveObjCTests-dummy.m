6ebe0b98df0dd1be308ec11e7e746da0567c013a
zhao chengfeng <zhaochengfengios@163.com>
2018-05-25 18:42:44 +0800

    添加gitignore

--- Pods/Target Support Files/Pods-AFNetworking+ReactiveObjCTests/Pods-AFNetworking+ReactiveObjCTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_AFNetworking_ReactiveObjCTests : NSObject
-@end
-@implementation PodsDummy_Pods_AFNetworking_ReactiveObjCTests
-@end
--- Pods/Target Support Files/Pods-AFNetworking+ReactiveObjCUITests/Pods-AFNetworking+ReactiveObjCUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_AFNetworking_ReactiveObjCUITests : NSObject
-@end
-@implementation PodsDummy_Pods_AFNetworking_ReactiveObjCUITests
-@end
