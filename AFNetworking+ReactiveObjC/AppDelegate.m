b107ff2d8932a9695db4da0ff42cefb3ca1ab7bf
赵成峰 <zhaochengfengios@163.com>
2018-09-07 11:09:30 +0800

    add request progress

--- AFNetworking+ReactiveObjC/AppDelegate.m
+++ AFNetworking+ReactiveObjC/AppDelegate.m
@@ -8,6 +8,7 @@
 
 #import "AppDelegate.h"
 #import "RACAFNetworking.h"
+#import "ViewController.h"
 
 @interface AppDelegate ()
 
@@ -18,6 +19,13 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
+    self.window.backgroundColor = [UIColor whiteColor];
+    [self.window makeKeyAndVisible];
+    
+    self.window.rootViewController = [[ViewController alloc] init];
+    
+    
     return YES;
 }
 
